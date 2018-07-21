8481ae5bf8e8455d21bbc4bbb2cb584ecad501cb
Divik Mittal <33899376+divik544@users.noreply.github.com>
2018-07-21 16:39:37 +0530

    added bellman_ford.cpp

---
+++ bellman_ford.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+const int INF = 0x3f3f3f3f;
+int main()
+{
+	int V;
+	cout << "Enter No. of vertices: ";
+	cin >> V;
+	// edge list
+	vector< pair<pair<int,int>,int> > edges;
+	int u,v,w;
+	cout << "Enter Edges(source destination weight)(-1 -1 -1 to terminate)\n";
+	while(1)
+	{
+		cin >> u >> v >> w;
+		if(u == -1)
+			break;
+		edges.push_back({{u,v},w});
+	}
+	//initialize distances with infinity
+	vector<int> dis(V,INF);
+	int src;
+	cout << "Enter source: ";
+	cin >> src;
+	dis[src] = 0;
+
+	// relax edges |V|-1 times
+	for(int i = 0; i < V-1; i++)
+	{
+		for(auto &edge : edges)
+		{
+			u = edge.first.first;
+			v = edge.first.second;
+			w = edge.second;
+			if(dis[v] > dis[u]+w)
+				dis[v] = dis[u]+w;
+		}
+	}
+
+	for(auto &edge : edges)
+	{
+		u = edge.first.first;
+		v = edge.first.second;
+		w = edge.second;
+		if(dis[v] > dis[u]+w)
+		{
+			cout << "Graph contains negative weight cycle." << endl;
+			return 0;
+		}
+	}
+	for(int i = 0; i < V; i++)
+		cout << "Distance["<<i<<"] is "<< dis[i] << '\n';
+}

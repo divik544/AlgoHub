45155488bc9a0c9390f0256b9bf143d00e2ae80d
divik544 <divikmittal544@gmail.com>
2019-02-02 09:10:17 +0530

    Corrected indentation

--- LinkedList/loop_detection.cpp
+++ LinkedList/loop_detection.cpp
@@ -18,14 +18,14 @@ bool detectloop(Node *head)
 	if(head == nullptr)
 		return false;
    
-   Node *slow,*fast;
-   slow = fast = head;
-   while(fast != nullptr && fast->next != nullptr)
-   {
+	Node *slow,*fast;
+	slow = fast = head;
+	while(fast != nullptr && fast->next != nullptr)
+	{
 		fast = fast->next->next;
 		slow = slow->next;
 		if(fast == slow)
 			return true;
-   }
-   return false;
+	}
+	return false;
 }

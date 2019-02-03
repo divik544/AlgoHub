e7547e2e1b8eb0147b5e66eeee47ed605edd56ff
divik544 <divikmittal544@gmail.com>
2019-02-03 10:13:23 +0530

    Improved formatting

--- LinkedList/reverseO(1).cpp
+++ LinkedList/reverseO(1).cpp
@@ -15,32 +15,32 @@ struct Node
 // Reversing LL using 3 extra pointers
 Node* reverse(Node *head)
 {
-    if(head == NULL)
-        return head;
-    Node *temp,*start = head,*end = head->next;
-    while(end != NULL)
-    {
-        temp = end->next;
-        end->next = head;
-        head = end;
-        start->next = temp;
-        end = temp;
-    }
-    return head;
+	if(head == NULL)
+		return head;
+	Node *temp,*start = head,*end = head->next;
+	while(end != NULL)
+	{
+		temp = end->next;
+		end->next = head;
+		head = end;
+		start->next = temp;
+		end = temp;
+	}
+	return head;
 }
 
 /* Reversing LL using 2 extra pointers
 Node* reverse(Node *head)
 {
-    if(head == NULL)
-        return head;
-    Node *temp,*start = head;
-    while(start->next != NULL)
-    {
-        temp = start->next->next;
-        start->next->next = head;
-        head = start->next;
-        start->next = temp;
-    }
-    return head;
+	if(head == NULL)
+		return head;
+	Node *temp,*start = head;
+	while(start->next != NULL)
+	{
+		temp = start->next->next;
+		start->next->next = head;
+		head = start->next;
+		start->next = temp;
+	}
+	return head;
 }*/

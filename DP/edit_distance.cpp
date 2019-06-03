ec01f1093938df60bf8865df726a8aaba765e5fb
Divik Mittal <divikmittal544@gmail.com>
2019-06-03 08:54:17 +0530

    added recursive edit distance

--- DP/edit_distance.cpp
+++ DP/edit_distance.cpp
@@ -1,3 +1,4 @@
+// iterative
 int Solution::minDistance(string A, string B) {
     int n = A.size();
     int m = B.size();
@@ -25,4 +26,28 @@ int Solution::minDistance(string A, string B) {
     	}
     }
     return dp[n][m];
+}
+
+// recursive
+int solve(vector<vector<int> > &dp, string &a, string &b, int i, int j)
+{
+    if(i == 0)
+    return j;
+    if(j == 0)
+    return i;
+    if(dp[i][j] != -1)
+    return dp[i][j];
+    int x = solve(dp,a,b,i-1,j-1);
+    if(a[i-1] == b[j-1])
+    return dp[i][j] = x;
+    
+    dp[i][j] = min({solve(dp,a,b,i-1,j),solve(dp,a,b,i,j-1),x})+1;
+    return dp[i][j];
+}
+int Solution::minDistance(string a, string b) {
+    int n = a.size();
+    int m = b.size();
+    vector<vector<int> > dp(n+1,vector<int>(m+1,-1));
+    int ans = solve(dp,a,b,n,m);
+    return ans;
 }
